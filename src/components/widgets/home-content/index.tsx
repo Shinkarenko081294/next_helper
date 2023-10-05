3a9506a7f6a0a39048cb31b6da8c18f273e7cbd6
shinkar90 <asuborisov@gmail.com>
2023-10-05 09:31:03 +0300

    feat: Closes #5 add new query

--- src/components/widgets/home-content/index.tsx
+++ src/components/widgets/home-content/index.tsx
@@ -1,4 +1,10 @@
+'use client'
+import {getUserData} from "@/app/api/api-query/getUserData";
+import useSWR from "swr";
+
 export const HomeContent = () =>{
+    const {data:any} = useSWR("userData", getUserData)
+    // console.log(data)
     return(
         <>This home content</>
     )
